417612f863339d3fc66188a97cb5796f4823b704
sven1103 <sven.fillinger@gmail.com>
2017-08-24 14:37:08 +0200

    Register entity and bio sample in openBIS

--- src/main/java/life/qbic/BarcodeRequestModelImpl.java
+++ src/main/java/life/qbic/BarcodeRequestModelImpl.java
@@ -8,7 +8,9 @@ import life.qbic.helpers.Utils;
 import life.qbic.openbis.openbisclient.OpenBisClient;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static life.qbic.helpers.BarcodeFunctions.checksum;
 
@@ -16,9 +18,11 @@ public class BarcodeRequestModelImpl implements BarcodeRequestModel{
 
     private final OpenBisClient openBisClient;
 
+    private final static String SPACE = "UKT_DIAGNOSTICS";
+
     private final static String PROJECTID = "/UKT_DIAGNOSTICS/QUK17";
 
-    private final static String CODEPREFIX = "QUK17";
+    private final static String CODE = "QUK17";
 
     private static Log log = LogFactoryUtil.getLog(MyPortletUI.class);
 
@@ -37,19 +41,109 @@ public class BarcodeRequestModelImpl implements BarcodeRequestModel{
 
         int numberOfNonEntitySamples = sampleList.size() - entities.size();
 
-        String biologicalSampleCode = createBarcodeFromInteger(numberOfNonEntitySamples);
-        String testSampleCode = createBarcodeFromInteger(numberOfNonEntitySamples + 1);
-        String patientId = CODEPREFIX + "ENTITY-" + (entities.size() + 1);
+        // offset is +2, because there is always an attachment sample per project
+        String biologicalSampleCode = createBarcodeFromInteger(numberOfNonEntitySamples + 2 );
+
+        // offset is +3, because of the previous created sample and the attachement
+        String testSampleCode = createBarcodeFromInteger(numberOfNonEntitySamples + 3);
+        String patientId = CODE + "ENTITY-" + (entities.size() + 1);
 
         idPair[0] = patientId;
         idPair[1] = testSampleCode;
 
         // Logging code block
         log.debug(String.format("Number of non-entity samples: %s", numberOfNonEntitySamples));
-        log.info(String.format("%s: New patient ID is %s", AppInfo.getAppInfo(), idPair[0]));
-        log.info(String.format("%s: New sample ID is %s", AppInfo.getAppInfo(), idPair[1]));
+        log.info(String.format("%s: New patient ID created %s", AppInfo.getAppInfo(), idPair[0]));
+        log.info(String.format("%s: New sample ID created %s", AppInfo.getAppInfo(), idPair[1]));
+
+        // In case registration fails, return null
+        if(!registerNewPatient(patientId, biologicalSampleCode, testSampleCode))
+            return null;
 
         return idPair;
+    }
+
+    /**
+     * Registration of a new patient with samples
+     * @param patientId A code for the sample type Q_BIOLOGICAL_ENTITY
+     * @param biologicalSampleCode A code for the sample type Q_BIOLOGICAL_SAMPLE
+     * @param testSampleCode A code for the sample type Q_TEST_SAMPLE
+     * @return True, if registration was successful, else false
+     */
+    private boolean registerNewPatient(String patientId, String biologicalSampleCode, String testSampleCode) {
+
+        registerEntity(patientId);
+        registerBioSample(biologicalSampleCode, "/"+SPACE+"/"+patientId);
+
+        return false;
+    }
+
+    /**
+     * Registration of a new biological sample
+     * @param biologicalSampleCode A code for the sample type Q_BIOLOGICAL_SAMPLE
+     * @param parent A code for the parent sample type Q_BIOLOGICAL_ENTITY
+     * @return True, if registration was successful, else false
+     */
+    private boolean registerBioSample(String biologicalSampleCode, String parent) {
+        Map<String, Object> params = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> metadata = new HashMap<>();
+        List<String> parents = new ArrayList<>();
+
+        metadata.put("Q_PRIMARY_TISSUE", "Other");
+        metadata.put("Q_TISSUE_DETAILED", "unknown");
+        parents.add(parent);
+
+        map.put("code", biologicalSampleCode);
+        map.put("space", SPACE);
+        map.put("project", CODE);
+        map.put("experiment", CODE + "E2");
+        map.put("type", "Q_BIOLOGICAL_SAMPLE");
+        map.put("metadata", metadata);
+        map.put("parents", parents);
+
+        params.put(biologicalSampleCode, map);
+
+        try{
+            openBisClient.ingest("DSS1", "register-sample-batch", params);
+        } catch (Exception exc){
+            log.error(exc);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Registration of a new test sample
+     * @param patientId A code for the sample type Q_BIOLOGICAL_ENTITY
+     * @return True, if registration was successful, else false
+     */
+    private boolean registerEntity(String patientId){
+        Map<String, Object> params = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> metadata = new HashMap<>();
+
+        metadata.put("Q_NCBI_ORGANISM", "9606");
+
+        map.put("code", patientId);
+        map.put("space", SPACE);
+        map.put("project", CODE);
+        map.put("experiment", CODE + "E1");
+        map.put("type", "Q_BIOLOGICAL_ENTITY");
+        map.put("metadata", metadata);
+
+
+        params.put(patientId, map);
+
+        try{
+            openBisClient.ingest("DSS1", "register-sample-batch", params);
+        } catch (Exception exc){
+            log.error(exc);
+            return false;
+        }
+
+        return true;
     }
 
 
@@ -89,7 +183,7 @@ public class BarcodeRequestModelImpl implements BarcodeRequestModel{
             return null;
         }
 
-        String preBarcode = CODEPREFIX + Utils.createCountString(remainingCounter, 3) + letter;
+        String preBarcode = CODE + Utils.createCountString(remainingCounter, 3) + letter;
 
         String barcode = preBarcode + checksum(preBarcode);
 
